9a708ac794ab3d61df6c2bafeec34400c49c2491
ltsambo <aung@ltsambo.co.kr>
2024-04-11 14:29:51 +0800

    create, update, delete functions and pages added

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using MagicVilla_Utility;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -22,11 +24,87 @@ namespace MagicVilla_Web.Controllers
             List<VillaDTO> list = new();
 
             var response = await _villaService.GetAllAsync<APIResponse>("");
-            if (response != null && response.IsSuccess) 
-            { 
+            if (response != null && response.IsSuccess)
+            {
                 list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
             }
             return View(list);
         }
-    }
+
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var response = await _villaService.CreateAsync<APIResponse>(model, "");
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Villa created successfully";
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            TempData["error"] = "Error encountered.";
+            return View(model);
+        }
+
+        public async Task<IActionResult> UpdateVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId, "");
+            if (response != null && response.IsSuccess)
+            {
+
+                VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(_mapper.Map<VillaUpdateDTO>(model));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["success"] = "Villa updated successfully";
+                var response = await _villaService.UpdateAsync<APIResponse>(model, "");
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            TempData["error"] = "Error encountered.";
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteVilla(int villaId)
+		{
+			var response = await _villaService.GetAsync<APIResponse>(villaId, "");
+			if (response != null && response.IsSuccess)
+			{
+				VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+				return View(model);
+			}
+			return NotFound();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteVilla(VillaDTO model)
+		{
+			var response = await _villaService.DeleteAsync<APIResponse>(model.Id, "");
+			if (response != null && response.IsSuccess)
+			{
+				TempData["success"] = "Villa deleted successfully";
+				return RedirectToAction(nameof(IndexVilla));
+			}
+			TempData["error"] = "Error encountered.";
+			return View(model);
+		}
+	}
 }
